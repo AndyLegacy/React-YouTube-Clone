307ab570640220886df21f2593f130cc45df7b83
AndyLegacy <andystav91@googlemail.com>
2018-01-31 19:57:59 +0000

    working

--- andytube/src/index.js
+++ andytube/src/index.js
@@ -1,8 +1,53 @@
-import React from 'react';
+import _ from 'lodash';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import YTSearch from 'youtube-api-search';
+import VideoList from './components/VideoList';
+import VideoDetail from './components/VideoDetails';
+import SearchBar from './components/SearchBar';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const API_KEY = 'AIzaSyBLo-wFMjQMpjdRNkwjHQbPvFQyhHXc3ng';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    };
+
+    this.videoSearch('napoli');
+
+  }
+
+  videoSearch(term) {
+    YTSearch({key: API_KEY, term: term }, (videos) => {
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0]
+      });
+    });
+  }
+
+  render() {
+    const videoSearch = _.debounce((term) => {
+      this.videoSearch(term);
+    }, 300);
+    return (
+      <div>
+        <SearchBar onSearchTermChange={videoSearch} />
+        <VideoDetail video={this.state.selectedVideo}/>
+        <VideoList
+          onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+          videos={this.state.videos} />
+      </div>
+    );
+  }
+}
+
+
+ReactDOM.render(
+  <App />,
+  document.querySelector('.container')
+);
